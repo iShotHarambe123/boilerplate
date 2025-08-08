d6ad34afb595f37ca0d36598a368b980a16ec746
iShotHarambe123 <tubbiedd@outlook.com>
2025-08-08 02:50:43 +0200

    feat: complete index page, add subpages, write CSS, and implement mobile menu JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
-// src/script.js
-console.log("Parcel fungerar!");
+
+// Visa år
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Accordion List
+document.querySelectorAll('.item-header').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const body = btn.nextElementSibling;
+    body.style.display = body.style.display === 'block' ? 'none' : 'block';
+  });
+});
+
+// Mobilmeny
+const menuBtn = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('show');
+});
